e38ce12778df9c827928175ba431a5d22d1e3a1d
Radu <radu@themeisle.com>
2018-03-13 16:25:12 +0200

    Enqueue custom login stylesheet

--- functions.php
+++ functions.php
@@ -290,3 +290,12 @@ function astell_login_logo() { ?>
     </style>
 <?php }
 add_action( 'login_enqueue_scripts', 'astell_login_logo' );
+
+//Add cutom-login-styles.css
+
+function astell_login_stylesheet() {
+    wp_enqueue_style( 'custom-login', get_stylesheet_directory_uri() . '/custom-login.css' );
+    //wp_enqueue_script( 'custom-login', get_stylesheet_directory_uri() . '/style-login.js' ); - TO USE IF I EVER
+    // WANT TO ADD EXTRA js TO THE LOGIN PAGE
+}
+add_action( 'login_enqueue_scripts', 'astell_login_stylesheet' );
